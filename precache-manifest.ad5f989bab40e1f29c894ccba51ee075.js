88185747c83cd04f064415c42160e9dc62b7ea9b
ilyapishchulin <ilyapishchulin@mail.ru>
2020-05-03 00:11:20 +0300

    Minor fix

---
+++ precache-manifest.ad5f989bab40e1f29c894ccba51ee075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34877d4eb95770c35e57",
+    "url": "./static/css/main.1caa2b3c.chunk.css"
+  },
+  {
+    "revision": "34877d4eb95770c35e57",
+    "url": "./static/js/main.741e3891.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c52ef62eb64fe78fb46f",
+    "url": "./static/css/2.3fd0f6bd.chunk.css"
+  },
+  {
+    "revision": "c52ef62eb64fe78fb46f",
+    "url": "./static/js/2.fea6daff.chunk.js"
+  },
+  {
+    "revision": "23d0287d020fc495c2aac32986966bda",
+    "url": "./index.html"
+  }
+];
